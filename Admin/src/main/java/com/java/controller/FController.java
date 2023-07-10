36b623a046cf3144aa9c9d1938aacc6241171c98
tyn66 <tyn66@DESKTOP-1PMF9D2>
2023-07-10 15:11:37 +0900

    0710 관리자 템플릿

---
+++ Admin/src/main/java/com/java/controller/FController.java
@@ -0,0 +1,259 @@
+package com.java.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class FController {
+
+	
+	
+	@RequestMapping("/calender")
+	public String calender() {
+		
+		return "calender";
+	}
+	
+	@RequestMapping("/chartjs")
+	public String chartjs() {
+		
+		return "chartjs";
+	}
+	@RequestMapping("/chartjs2")
+	public String chartjs2() {
+		
+		return "chartjs2";
+	}
+	
+	@RequestMapping("/contacts")
+	public String contacts() {
+		
+		return "contacts";
+	}
+	
+	@RequestMapping("/e_commerce")
+	public String e_commerce() {
+		
+		return "e_commerce";
+	}
+	
+	@RequestMapping("/echarts")
+	public String echarts() {
+		
+		return "echarts";
+	}
+	
+	@RequestMapping("/fixed_footer")
+	public String fixed_footer() {
+		
+		return "fixed_footer";
+	}
+	
+	@RequestMapping("/fixed_sidebar")
+	public String fixed_sidebar() {
+		
+		return "fixed_sidebar";
+	}
+	
+	@RequestMapping("/form_advanced")
+	public String form_advanced() {
+		
+		return "form_advanced";
+	}
+	
+	@RequestMapping("/form_buttons")
+	public String form_buttons() {
+		
+		return "form_buttons";
+	}
+	
+	
+	@RequestMapping("/form_upload")
+	public String form_upload() {
+		
+		return "form_upload";
+	}
+	
+	@RequestMapping("/form_validation")
+	public String form_validation() {
+		
+		return "form_validation";
+	}
+	
+	@RequestMapping("/form_wizards")
+	public String form_wizards() {
+		
+		return "form_wizards";
+	}
+	
+	@RequestMapping("/form")
+	public String form() {
+		
+		return "form";
+	}
+	
+	@RequestMapping("/general_elements")
+	public String general_elements() {
+		
+		return "general_elements";
+	}
+	
+	@RequestMapping("/glyphicons")
+	public String glyphicons() {
+		
+		return "glyphicons";
+	}
+	
+	@RequestMapping("/icons")
+	public String icons() {
+		
+		return "icons";
+	}
+	
+	@RequestMapping("/inbox")
+	public String inbox() {
+		
+		return "inbox";
+	}
+	
+	@RequestMapping("/index")
+	public String index() {
+		
+		return "index";
+	}
+	
+	@RequestMapping("/index2")
+	public String index2() {
+		
+		return "index2";
+	}
+	
+	@RequestMapping("/index3")
+	public String index3() {
+		
+		return "index3";
+	}
+	
+	@RequestMapping("/invoice")
+	public String invoice() {
+		
+		return "invoice";
+	}
+	
+	@RequestMapping("/level2")
+	public String level2() {
+		
+		return "level2";
+	}
+	
+	@RequestMapping("/login")
+	public String login() {
+		
+		return "login";
+	}
+	
+	@RequestMapping("/map")
+	public String map() {
+		
+		return "map";
+	}
+	
+	@RequestMapping("/media_gallery")
+	public String media_gallery() {
+		
+		return "media_gallery";
+	}
+	
+	@RequestMapping("/morisjs")
+	public String morisjs() {
+		
+		return "morisjs";
+	}
+	
+	@RequestMapping("/other_charts")
+	public String other_charts() {
+		
+		return "other_charts";
+	}
+	
+	@RequestMapping("/page_403")
+	public String page_403() {
+		
+		return "page_403";
+	}
+	
+	@RequestMapping("/page_404")
+	public String page_404() {
+		
+		return "page_404";
+	}
+	
+	@RequestMapping("/page_500")
+	public String page_500() {
+		
+		return "page_500";
+	}
+	
+	@RequestMapping("/plain_page")
+	public String plain_page() {
+		
+		return "plain_page";
+	}
+	@RequestMapping("/pricing_tables")
+	public String pricing_tables() {
+		
+		return "pricing_tables";
+	}
+	
+	@RequestMapping("/profile")
+	public String profile() {
+		
+		return "profile";
+	}
+	
+	@RequestMapping("/project_detail")
+	public String project_detail() {
+		
+		return "project_detail";
+	}
+	
+	@RequestMapping("/projects")
+	public String projects() {
+		
+		return "projects";
+	}
+	
+	@RequestMapping("/tables_dynamic")
+	public String tables_dynamic() {
+		
+		return "tables_dynamic";
+	}
+	
+	@RequestMapping("/tables")
+	public String tables() {
+		
+		return "tables";
+	}
+	@RequestMapping("/typography")
+	public String typography() {
+		
+		return "typography";
+	}
+	
+	@RequestMapping("/widgets")
+	public String widgets() {
+		
+		return "widgets";
+	}
+	
+	@RequestMapping("/xx")
+	public String xx() {
+		
+		return "xx";
+	}
+	
+	
+	
+	
+	
+}
