1aefb26da16554b96a265541b8a8094ee189c19a
05-09 <05-09@DESKTOP-3Q0ND47>
2023-07-12 14:30:51 +0900

    .

--- ZEBRA/src/main/java/com/java/dao/MemberDAO.java
+++
@@ -1,7 +0,0 @@
-package com.java.dao;
-
-import com.java.dto.MemberDTO;
-
-public interface MemberDAO {
- public String loginCheck(MemberDTO dto);
-} 
--- ZEBRA/src/main/java/com/java/service/MemberService.java
+++
@@ -1,9 +0,0 @@
-package com.java.service;
-
-import javax.servlet.http.HttpSession;
-import com.java.dto.MemberDTO;
-
-public interface MemberService {
- public String loginCheck(MemberDTO dto, HttpSession session);
- public void logout(HttpSession session);
-}
