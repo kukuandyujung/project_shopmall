4b024bd77d028aed74ef530890d451e799710f21
05-09 <05-09@DESKTOP-3Q0ND47>
2023-07-20 13:17:48 +0900

    .

---
+++ ZEBRA/src/main/java/com/java/service/CommentService.java
@@ -0,0 +1,8 @@
+package com.java.service;
+
+import java.util.ArrayList;
+
+import com.java.dto.CommentDto;
+
+public interface CommentService {
+}
---
+++ ZEBRA/src/main/java/com/java/service/CommentServiceImpl.java
@@ -0,0 +1,13 @@
+package com.java.service;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.java.dto.BoardDto;
+import com.java.dto.CommentDto;
+import com.java.mapper.BoardMapper;
+
+public class CommentServiceImpl implements CommentService {
+
+}
